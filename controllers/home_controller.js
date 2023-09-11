f4152d56a0b46d825fd42fb5da1400a1d5d21381
ATULKUMARBARANWAL <92468670+ATULKUMARBARANWAL@users.noreply.github.com>
2023-09-12 01:28:29 +0530

    adding flash meassage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,34 +1,59 @@
-const Post=require('../models/post');
-const User=require('../models/user')
-module.exports.home=function(req,res){
-    // console.log(req.cookies);
-    // Post.find({},function(err,posts)
-    // {
-    //     return res.render('home',{
-    //     title:"Codeial|Home",
-    //     posts:posts
-    // }); 
-    // })
-   Post.find({})
-   .populate('user')
-   .populate({
-  path:'comments',
-  populate:{
-   path:'user'
-  }
-})
-   .exec(function(err,posts)
-   {
-   User.find({},function(err,users)
-   {
-      return res.render('home',{
-        title:"Codeial|Home",
-        posts:posts,
-        all_users:users
-   })
+const Post = require("../models/post");
+const User = require("../models/user");
+// module.exports.home=function(req,res){
+// console.log(req.cookies);
+// Post.find({},function(err,posts)
+// {
+//     return res.render('home',{
+//     title:"Codeial|Home",
+//     posts:posts
+// });
+// })
+//.populate ka mtlb fetching all data from the database normally only refrence fetch kr paata hai
+// module.exports.home = function (req, res) {
+//   Post.find({})
+//     .populate("user")
+//     .populate({
+//       path: "comments",
+//       populate: {
+//         path: "user",
+//       },
+//     })
+//     .exec(function (err, posts) {
+//       User.find({}, function (err, users) {
+//         return res.render("home", {
+//           title: "Codeial|Home",
+//           posts: posts,
+//           all_users: users,
+//         });
+//       });
+//     });
+// };
+//
 
-   
-    
-   }) 
-})
+//usig promises
+module.exports.home=async function(req,res)
+{
+   try{
+   let posts= await Post.find({})
+   .populate("user")
+   .populate({
+     path: "comments",
+     populate: {
+       path: "user",
+     },
+   })
+   let users=await User.find({})
+   {
+      return res.render("home", {
+        title: "Codeial|Home",
+        posts: posts,
+        all_users: users,
+      });
+    }
+   }
+    catch(err){
+      console.log("error",err);
+      return;
+    }
 }
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,64 +1,52 @@
-const Post=require('../models/post');
-const Comment=require('../models/comment');
-module.exports.create=function(req,res)
-{
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
-    },
-   function(err,post)
-   {
-    if(err)
-    {
-        console.log('error in creating post',err);
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+module.exports.create = async function (req, res) {
+  try {
+    await Post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
+
+    return res.redirect("back");
+  } catch (err) {
+    console.log("err", err);
     return;
-    }
-    return res.redirect('back');
-   } 
-    )
-}
+  }
+};
 
 // module.exports.destroy= async function(req,res)
 // {
 //   const post=await Post.findByIdAndDelete(req.params.id)
 //                 console.log(post.user);
 //                 console.log(req.user.id);
-//      //.id convert the id into the string form 
+//      //.id convert the id into the string form
 //      await Comment.deleteMany({
 //         post:req.params.id                                             *ye bhi shi hai
-        
+
 //      }
 //      )
 //      return res.redirect('back');
 //     }
-     
-    
-    
-    module.exports.destroy = async function (req, res) {
-      try {
-        const post = await Post.findById(req.params.id);
-    
-        if (!post) {
-          return res.status(404).send('Post not found'); // Handle the case where the post is not found
-        }
-    
-        // Check if the user is the author of the post
-        if (post.user.toString() === req.user.id) {
-          await post.remove();
-          await Comment.deleteMany({ post: req.params.id });
-    
-          return res.redirect('back');
-        } else {
-          return res.status(403).send('You are not authorized to delete this post');
-        }
-      } catch (err) {
-        console.error(err);
-        return res.status(500).send('Internal Server Error'); // Handle other errors
-      }
-    };
-           
-    
-   
 
-   
-        
+module.exports.destroy = async function (req, res) {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).send("Post not found"); // Handle the case where the post is not found
+    }
+
+    // Check if the user is the author of the post
+    if (post.user.toString() === req.user.id) {
+      await post.remove();
+      await Comment.deleteMany({ post: req.params.id });
+
+      return res.redirect("back");
+    } else {
+      return res.status(403).send("You are not authorized to delete this post");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error"); // Handle other errors
+  }
+};
