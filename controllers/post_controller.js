b9982ce9272f82855d427ec246cae941cbaa71b2
ATULKUMARBARANWAL <92468670+ATULKUMARBARANWAL@users.noreply.github.com>
2023-09-13 22:30:47 +0530

    Creating post and reciving data

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,11 +1,22 @@
 const Post = require("../models/post");
 const Comment = require("../models/comment");
+const { post } = require("../routes");
 module.exports.create = async function (req, res) {
   try {
-    await Post.create({
+    let post=await Post.create({
       content: req.body.content,
       user: req.user._id,
+
     });
+    if(req.xhr)
+    {
+      return res.status(200).json({
+        data:{
+          post:post
+        },
+        message:"this.created !"
+      })
+    }
     req.flash('success','Post published')
     return res.redirect("back");
   } catch (err) {
