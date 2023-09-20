2aec13135c26c45ef0f4304f13a1e0607a672b17
ATULKUMARBARANWAL <92468670+ATULKUMARBARANWAL@users.noreply.github.com>
2023-09-20 19:22:52 +0530

    Delete a post without reloding or using ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,31 +1,66 @@
 console.log("helllo");
-{//method to submit the form data for new post using ajax
-    let createPost=function()
-    {
-        let newPostForm=$('#new-post-form');
-        newPostForm.submit(function(e)
-        {
-            e.preventDefault();
-            $.ajax({
-                type:'post',
-                url:'/posts/create',
-                data:newPostForm.serialize(),
-                success:function(data)
-                {
-                    console.log(data)
-                },
-                error:function(error)
-                {
-                    console.log(error.responseText);
-                }
-            })
-        });
-        
-    }
-    //method to create a post in DOM
- 
+{
+  //method to submit the form data for new post using ajax
+  let createPost = function () {
+    let newPostForm = $("#new-post-form");
+    newPostForm.submit(function (e) {
+      e.preventDefault();
+      $.ajax({
+        type: "post",
+        url: "/posts/create",
+        data: newPostForm.serialize(),
+        success: function (data) {
+          let newPostDom=newPostDom(data.data.post);
+          $('#posts-list-container').prepend(newPost);
+        },
+        error: function (error) {
+          console.log(error.responseText);
+        },
+      });
+    });
+  };
+  //method to create a post in DOM
+  //creating a function in dom for converting html part which we have copy from _post.ejs to DOM
+  let newPostDom = function (post) {
+    return $(` <ul id="post-${post._id}">  // id is for deleting the post 
+      
+        <%for(post of posts){%>
+          <li>
+            <p>
+             
+              <small>
+                    <a href="/posts/destroy/${ post.id }" class="delete-post-button"> //for attach common styling and add click listinor to it
+                  <!-- <i class="fa-duotone fa-delete-right"></i>
+                   -->
+                   X
+                </a>
+              </small>
+              ${post.content }
+              <br />
+              <small> ${ post.user.name }</small>
+            </p>
+    
+            <div class="post-comments">
+              
+    
+              <form action="/comments/create" method="POST">
+                <input
+                  type="text"
+                  name="content"
+                  placeholder="write comment here..." required
+                />
+                <input type="hidden" name="post" value="${ post.id }" />
+                <input type="submit" name="Add Comment" />
+              </form>
+              
+              <div class="post-comments-list">
+             <ul id=post-comments-${post._id}>
+             </ul>
+              </div>
+            </div>
+          </li>`);
+  };
 
-    createPost();
+  createPost();
 }
 //
-   
