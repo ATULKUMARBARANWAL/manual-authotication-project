6ed690b27d06cde9a000fea59b654371c9edf1ca
ATULKUMARBARANWAL <92468670+ATULKUMARBARANWAL@users.noreply.github.com>
2023-12-05 23:36:05 +0530

    jws_web_token

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,3 +1,16 @@
 const passport=require('passport')
 const JWTStrategy=require('passport-jwt').Strategy;
 const ExtractJWT=require('passport-jwt').ExtractJwt;//it helps to extract jwt from header..
+const User=require('../models/user');
+let opts = {}
+opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'codeial';
+passport.use(new JWTStrategy(opts, async function(jwtPayLoad, done){
+    let user = await User.findById(jwtPayLoad._id);
+    if(user){
+        return done(null, user);
+    }else{
+        return done(null, false);
+    }
+}))
+module.export = passport;
